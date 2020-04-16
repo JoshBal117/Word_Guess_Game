e55b198dd67e5b0f8fecb66ea4c7e0dd488aa485
Joshua Balao <joshuacbalao@gmail.com>
2020-04-15 20:28:45 -0400

    add guessable words for Hangman game and add random word generator

---
+++ assets/javascript/game.js
@@ -0,0 +1,15 @@
+var words= [
+    "storm",
+    "cyclops",
+    "spider-man",
+    "captain america",
+    "thor",
+    "iron Man",
+    "hulk",
+    "captain marvel",
+    "wolverine",
+    "black widow",
+    "black panther",
+]
+
+var word = words[Math.floor(Math.random() * words.length)];
