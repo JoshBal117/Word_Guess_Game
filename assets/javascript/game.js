7bdc48caa250a554f909ec87c22390e999be9918
Joshua Balao <joshuacbalao@gmail.com>
2020-04-18 23:11:50 -0400

    add keylog function to record user guesses and edit html page by removing useless text

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,6 @@
 //Then to have a randomizer array to randomly select a word
 //Next step is set up a blank array to for the guessable word. 
 //After that we will need to have the emtpy spaces update after a correct guess
-//
 
 let words= [
     "storm",
@@ -10,7 +9,7 @@ let words= [
     "spiderman",
     "captainamerica",
     "thor",
-    "ironMan",
+    "ironman",
     "hulk",
     "captainmarvel",
     "wolverine",
@@ -27,14 +26,6 @@ let words= [
     "magneto",
 
 ]
-//setting up a onkeydown function to log key presses
-// const input = document.querySelector('input')
-// const log = document.getElementById('log')
-// input.onkeydown = function logKey(e) {
-//     log.textContent += ' ${e.code}';
-// }
-
-
 //set a string for generating the random word from
 //a list already provided. 
 let word = words[Math.floor(Math.random() * words.length)];
@@ -55,31 +46,25 @@ console.log(answerArray.join(" "))
 
 //Need to have user input their guess into the game
 
+let wrongGuess =[]
 
 
+let guessesLeft = 20;
 
-//possible solution?
-// while(remainingLetters > 0) {
-//     console.log(answerArray.join(" "))
-//     //ensure that the user's guess will appear
-//     let guess = input.onkeydown
-//         console.log("THIS WORKED!!")
-//     if(guess === null) {
-//         break;
-//         console.log("this is working too!")
-//     } else if (guess.length !== 1) {
-//         //updating the game state with a guess
-//             console.log("this worked too!");
-//         } else {for (var j=0; j < word.length; j++) {
-//             if (word[j] === guess) {
-//                 answerArray[j] = guess;
-//                 remainingLetters--;
-//             }
-//         }
-//     }
-// }
+//
+document.onkeydown = function(event){
+  for (j = 0; j < word.length; j++){
+    if(event.key === word.charAt(j)){
+      answerArray.splice(j, 1, event.key)
+      console.log(answerArray.join (" "))
+    }
+  }
+}
 
-//set up a number of guesses to 15 guesses before game states a failure notification
+if(guessesLeft <= 0){
+  console.log("Don't choke on your aspirations!-Vader")
+}
+//set up a number of guesses to 20 guesses before game states a failure notification
 
 //To have, once user guesses the character; an image of the character and a sound byte to play at the same time
 
